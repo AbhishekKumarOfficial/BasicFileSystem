ace4d2c732a0d4efbdb39f150b5ab039005037b2
Abhishek Kumar <officialabhishek@outlook.in>
2022-05-15 01:24:00 +0530

    Added structure of project. Minimal functionality.

---
+++ src/scala/commands/Command.scala
@@ -0,0 +1,13 @@
+package scala.commands
+
+import scala.filesystem.State
+
+trait Command {
+  def apply(state: State): State
+
+}
+
+object Command {
+  def from(input: String): Command =
+    new UnknownCommand
+}
--- src/scala/filesystem/FileSystem.scala
+++ src/scala/filesystem/FileSystem.scala
@@ -1,11 +1,18 @@
 package scala.filesystem
 
 import java.util.Scanner
+import scala.commands.Command
+import scala.files.Directory
 
 object FileSystem extends App {
+
+  val root = Directory.ROOT
+  var state = State(root, root)
   val scanner = new Scanner(System.in)
+
   while(true) {
-    print("$ ")
-    println(scanner.nextLine())
+    state.show
+    val input = scanner.nextLine()
+    state = Command.from(input).apply(state)
   }
 }
